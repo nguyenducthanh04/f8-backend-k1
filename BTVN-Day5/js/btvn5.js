4a503408b7adb8130033d261247c0c91c87cf221
your name <dducthanh04@gmail.com>
2023-07-12 16:21:14 +0700

    Nop bai tap

--- BTVN-Day5/js/btvn5.js
+++ BTVN-Day5/js/btvn5.js
@@ -1,17 +1,46 @@
-var music = "Vì ngày em đẹp nhất, là ngày anh mất em, dù đớn đau như vậy, đành chấp nhận ta chỉ đến đây. Mong em hạnh phúc nửa đời về sau, đâu nhất thiết phải cùng nhau.";
-var keyWord = music.split(" ");
-var myIndex = 0;
-var colorKey = function() {
-  var discolorationKey = keyWord.map(
-    function(keyword, index) {
-      if(index === myIndex) {
-        return "<span>" + keyword + "</span>";
-      }
-      else {
-        return keyword;
+// var music = "Vì ngày em đẹp nhất, là ngày anh mất em, dù đớn đau như vậy, đành chấp nhận ta chỉ đến đây. Mong em hạnh phúc nửa đời về sau, đâu nhất thiết phải cùng nhau.";
+// var keyWord = music.split(" ");
+// var myIndex = 0;
+// var colorKey = function() {
+//   var discolorationKey = keyWord.map(
+//     function(keyword, index) {
+//       if(index === myIndex) {
+//         return "<span>" + keyword + "</span>";
+//       }
+//       else {
+//         return keyword;
+// }
+//     });
+//     document.getElementById("karaoke").innerHTML = discolorationKey.join(" ");
+//     myIndex = (myIndex + 1) % keyWord.length;
+// };
+// setInterval(colorKey, 1000);
+//  var luongcoban, thue, luongthucnhan;
+//  var luongcoban = 5;
+//  if(luongcoban <= 5) {
+//   luongthucnhan = luongcoban - (luongcoban * 5) / 100;
+//  }
+//  else if(luongcoban >= 5 && luongcoban <= 10) {
+//   luongthucnhan = luongcoban - (luongcoban * 7) / 100;
+//  }
+//  else if(luongcoban > 10) {
+//   luongthucnhan = luongcoban - (luongcoban * 10) / 100;
+//  }
+//  console.log(`Lương sau khi trừ thuế đi là: ${luongthucnhan}`);
+
+var dtb = 3;
+if(dtb < 5) {
+    console.log("Xếp loại học lực yếu");
 }
-    });
-    document.getElementById("karaoke").innerHTML = discolorationKey.join(" ");
-    myIndex = (myIndex + 1) % keyWord.length;
-};
-setInterval(colorKey, 1000);
+else if(dtb >= 5 && dtb < 6) {
+    console.log("Xếp loại học lực trung bình");
+}
+else if(dtb >= 6.5 && dtb < 8) {
+    console.log("Xếp loại học lực khá");
+}
+else if(dtb >= 8 && dtb < 10) {
+    console.log("Xếp loại học lực giỏi");
+}
+else if(dtb == 10) {
+    console.log("Xếp loại học lực xuất sắc");
+}
---
+++ BTVN_Day6/js/day6.js
@@ -0,0 +1,80 @@
+//Bài 1 
+ var myNumber = [1, 9, 6, 8, 12];
+ var max = myNumber[0];
+ var addressMax = [0];
+ var min = myNumber[0];
+ var addressMin = [0];
+ for(var i = 0; i < myNumber.length; i++) {
+    if(max < myNumber[i] ){
+        max = myNumber[i];
+        addressMax = [i];
+    }
+    if(min > myNumber[i]) {
+        min = myNumber[i];
+        addressMin = [i];
+    }
+ }
+ console.log(`Số lớn nhất là ${max} tại vị trí ${addressMax}`);
+ console.log(`Số nhỏ nhất là ${min} tại vị trí ${addressMin}`);
+
+//Bài 2
+var num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+function isNum(n) {
+    if(n < 2) {
+        return false;
+    }
+    for(var i = 1; i < Math.sqrt(n); i++) {
+    
+        if(n % 2 === 0) {
+           return false;
+       }
+   }
+           return true;
+   
+}
+function isArray(num) {
+    var S = 0;
+    var count = 0;
+ num.forEach(function(a){
+    if(isNum(a)){
+        S += a;
+        count++;
+    }
+});
+if(count === 0) {
+    return `Không có số nguyên tố`;
+}
+    return parseFloat(S / count);
+
+}
+console.log(`Trung bình số nguyên tố là: ${isArray(num)}`);
+
+//Bài 3
+
+var names = [
+    'Thanh',
+    'Đức',
+    'Thanh',
+    'Nam',
+]
+var newNames = [...new Set(names)];
+console.log(newNames);
+
+//Bài 4
+
+var numbers = [5, 1, 9, 8, 10];
+var element = 4;
+numbers.sort(function(a, b){
+    if(a > b) {
+        return 1;
+    }
+    return -1;
+});
+var i = 0;
+while (numbers[i] < element){
+    i++;
+}
+numbers.splice(i, 0, element);
+console.log(numbers);
+
+
