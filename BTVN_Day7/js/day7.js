d211dd1ccae566810311bba23efd4a070c4a75ac
your name <dducthanh04@gmail.com>
2023-07-19 10:20:08 +0700

    Nop bai tap

--- BTVN_Day7/js/day7.js
+++ BTVN_Day7/js/day7.js
@@ -31,3 +31,35 @@ var newArr = arr.reduce(function(prev, current){
     return prev;
 }, []);
 console.log(newArr);
+
+
+// var content = 'Chủ tịch UBND tỉnh Thừa Thiên-Huế Nguyễn Văn Phương báo cáo về tình tình thực hiện Nghị quyết Đại hội Đảng bộ tỉnh lần thứ XVI nhiệm kỳ 2020-2025 gắn với Nghị quyết Đại hội XIII của Đảng';
+// var customer = content.split(' ').slice(0, 24).join(' ');
+// console.log(customer);
+// document.write(customer);
+
+// //Vòng lặp reduce
+// var numbers = [2, 5, 7, 8, 9, 100];
+// var searchMax = numbers.reduce(function(prev, current){
+//   max = prev;//2
+//   if(prev < current){
+//     //2      //5
+//     max = current;//5
+//   } //Lần lượt lặp cho đến ptu cuối 
+//   return max;
+//   //Cách 2 dùng toán tử 3 ngôi
+//   //return prev > current ? prev : current;
+// });
+// console.log(searchMax);
+
+
+// var arr1 = [1, 2, 5, 7, 9];
+// var arr2 = [1, 5, 2];
+// var newArr = arr1.reduce(function(prev, current){
+//     // if(!arr2.includes(current)){
+//     //   prev.push(current);
+//     // }
+//     // return prev;
+//     return !arr2.includes(current) && prev.push(current), prev;
+// }, []);
+// console.log(newArr);
---
+++ BTVN_Day8/Day8.js
@@ -0,0 +1,81 @@
+//Bai 1
+function Customer(name, age, address) {
+    this.name = name;
+    this.age = age;
+    this.address = address;
+  }
+  
+  const memberOne = new Customer("Nguyễn Văn A", 11, "Ha Noi");
+  const memberTwo = new Customer("Nguyễn Văn B", 2, "Hai Phong");
+  const memberThree = new Customer("Nguyễn Văn C", 12, "TP.HCM");
+  
+  const customers = [
+    memberOne,
+    memberTwo,
+    memberThree
+  ];
+  
+  function createCustomers(customers) {
+    customers.forEach((customer) => {
+      const nameArr = customer.name.split(" ");
+      customer.shortName = nameArr[0] + " " + nameArr[nameArr.length - 1];
+    });
+    return customers.sort((memberOne, memberTwo) => memberOne.age - memberTwo.age);
+  }
+  
+  const result = createCustomers(customers);
+  console.log(result);
+
+//Bai 2
+
+const data = [];
+function User(name, email, password) {
+  this.name = name;
+  this.password = password;
+  this.email = email;
+}
+
+function handleRegister(name, email, password) {
+  this.name = name;
+  this.email = email;
+  this.password = password;
+
+  const result = data.filter(function (user) {
+    return user.email === email;
+  });
+
+  if (name === undefined || password === undefined || email === undefined) {
+    return "Dữ liệu không hợp lệ, đăng ký không thành công";
+  }
+  else if(result.length !== 0){
+    return "Tài khoản đã tồn tại";
+  } 
+  else {
+    console.log("Đăng ký thành công");
+    const user = new User(name, email, password);
+    user.role = "User";
+    data.push(user);
+    return data;
+  }
+}
+
+console.log(handleRegister("hehehe", 188290));
+console.log(handleRegister("Đức Thanh", "dducthanh04@gmai.conm", 79797979));
+console.log(handleRegister("Thanh Tùng", "thanhtung7979@gmail.conm", 86868686));
+console.log(handleRegister("Quốc Minh", "qminh04@gmai.conm", 69696969));
+
+function handleLogin(email, password) {
+  this.email = email;
+  this.password = password;
+
+  let result = data.filter(function (user) {
+    return user.email === email && user.password === password;
+  });
+
+  if (result.length === 0) {
+    return "Thông tin đăng nhập không hợp lệ";
+  } else return result;
+}
+
+console.log(handleLogin("abcgdds@gmail.comz", 5465646));
+console.log(handleLogin("xyzah@gmail.comy", 6565));
