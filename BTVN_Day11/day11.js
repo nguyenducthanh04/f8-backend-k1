039c77db3e4d7070db6db7a8e214ff07ab69b2a9
your name <dducthanh04@gmail.com>
2023-07-29 23:37:23 +0700

    Nop bai tap ve nha

---
+++ BTVN_Day11/day11.js
@@ -0,0 +1,20 @@
+/**Cho trước biến url lưu trữ URL cần rút gọn link. Viết chương trình hiển thị ra link sau khi rút gọn
+
+Khi truy cập vào link rút gọn sẽ tự động chuyển về URL gốc
+
+Tham khảo API sau để hoàn thiện: https://shrtco.de/docs */
+
+const url = `https://www.youtube.com/watch?v=WICgZPU3YoM`;
+const api = `https://api.shrtco.de/v2/shorten?url=`;
+
+const getShorten = async function (link) {
+  try {
+    const response = await fetch(`${api}${link}`);
+    const data = await response.json();
+    console.log(data.result.short_link);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+getShorten(url);
