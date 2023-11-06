a64f3dcf89d79bd48622d5a7fa76a62441893445
your name <dducthanh04@gmail.com>
2023-11-06 17:45:05 +0700

    update

--- BTVN38_Cache/controllers/UserController.js
+++ BTVN38_Cache/controllers/UserController.js
@@ -1,8 +1,29 @@
 const model = require("../models/index");
 const User = model.User;
+const Cache = require("file-system-cache").default;
+const cache = Cache({
+  basePath: "./.cache", // (optional) Path where cache files are stored (default).
+  ns: "f8", // (optional) A grouping namespace for items.
+  hash: "sha1", // (optional) A hashing algorithm used within the cache key.
+  ttl: 86400, // (optional) A time-to-live (in secs) on how long an item remains cached.
+});
+const cacheStore = async (key, value, ttl) => {
+  let data = await cache.get(key);
+  if (!data) {
+    cache.setSync(key, value, ttl);
+    data = value;
+    console.log("Database");
+  } else {
+    console.log("Cache");
+  }
+  return data;
+};
+
 module.exports = {
   index: async (req, res) => {
-    const user = await User.findAll();
-    res.send(user);
+    let users = await User.findAll();
+    const dataUser = await cacheStore("users", users, 3600);
+    console.log(dataUser);
+    res.render("users/index", { users });
   },
 };
--- BTVN38_Cache/routes/users.js
+++ BTVN38_Cache/routes/users.js
@@ -1,8 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const UserController = require("../controllers/UserController");
-const UserMiddleware = require("../middlewares/UserMiddleware");
 /* GET users listing. */
-router.get("/", UserMiddleware.cacheMiddleware, UserController.index);
+router.get("/", UserController.index);
 
 module.exports = router;
